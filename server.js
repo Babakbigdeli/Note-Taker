fe4b6fd3c558ba31da47e1d725949846beaec125
Babakbigdeli <babakbigdeli@gmail.com>
2021-01-24 11:43:06 +0000

    Birnging in Express requirements

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-
+//Birnging in Express requirements
+var app = express();
+var PORT = process.env.PORT || 3000;
